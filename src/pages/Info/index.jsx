37a7c9ed1eeb8e49a18747776f76d4bcc33986d0
Enzo Gabriel Guedes <eggqsaraiva@gmail.com>
2024-05-13 13:08:53 -0300

    Adicionando rodapé a página de informações

--- src/pages/Info/index.jsx
+++ src/pages/Info/index.jsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Heading, UnorderedList, ListItem } from '@chakra-ui/react'
+import { Flex, Text, Heading, UnorderedList, ListItem, Box, Link } from '@chakra-ui/react'
 import React from 'react'
 
 const Info = () => {
@@ -10,7 +10,7 @@ const Info = () => {
       <Text fontSize={"xl"} color={"secondary.400"} mb={4}>
         O SIPAV, Sistema de Controle e Lembrete de Vacinas, é uma aplicação desenvolvida com o propósito de facilitar o controle e o acompanhamento das vacinas necessárias para a população, além de fornecer lembretes úteis para garantir que as doses sejam tomadas conforme o calendário recomendado.
       </Text>
-      
+
       <Text fontSize={"xl"} color={"secondary.400"} mb={4}>
         Este sistema foi concebido como parte de um projeto de estudo, especificamente como trabalho de conclusão de curso (TCC). Ele visa explorar e aplicar conhecimentos relacionados ao desenvolvimento de aplicativos web, bem como abordar questões relevantes para a saúde pública, como a importância da vacinação regular.
       </Text>
@@ -29,6 +29,10 @@ const Info = () => {
       <Text fontSize={"xl"} color={"secondary.400"} mb={4}>
         É importante ressaltar que o SIPAV é uma ferramenta educativa e de estudo, desenvolvida com o intuito de contribuir para uma melhor compreensão sobre a importância da vacinação e para promover a saúde pública de forma geral. Embora seja uma aplicação fictícia, seu desenvolvimento e utilização podem fornecer insights valiosos sobre o desenvolvimento de soluções tecnológicas para questões de saúde.
       </Text>
+
+      <Box mt={16} textAlign="center" pb={4}>
+        <Text fontWeight={"bold"} fontSize={"xl"} color="secondary.400">Desenvolvido por <Link bg={"secondary.400"} href="https://github.com/enzoggqs" isExternal color="primary.500" >Enzo Gabriel</Link></Text>
+      </Box>
     </Flex>
   )
 }
